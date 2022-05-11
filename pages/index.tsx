2598ab48866bca06a585ced4a4c205067086e2cf
addy360 <abbyboy360@gmail.com>
2022-05-11 13:23:42 +0300

    printing system info

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,14 @@ import type { NextPage } from 'next'
 import { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react'
 import SockJS, { } from "sockjs-client"
 
+export interface SysInfo {
+  availableProcessor: number;
+  freeMemory: number;
+  maxMemory: number;
+  totalMemory: number;
+  time: Date
+}
+
 
 const Home: NextPage = () => {
 
@@ -11,13 +19,17 @@ const Home: NextPage = () => {
   const [compatClient, setCompatClient] = useState<CompatClient | undefined>()
   const [messages, setMessages] = useState<string[]>([])
   const [value, setValue] = useState<string>("")
+  const [sysInfo, setSysInfo] = useState<SysInfo>()
 
-  const ref = useRef<HTMLInputElement>()
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     compatClient?.send("/app/hi", {}, JSON.stringify({ message: value }))
     setValue("")
+  }
+
+  const getSystemInfo = () => {
+    compatClient?.send("/app/sys")
   }
 
   const handleConnect = () => {
@@ -30,6 +42,11 @@ const Home: NextPage = () => {
       c.subscribe("/message/greetings", (message) => {
         setMessages(smss => [JSON.parse(message.body).message, ...smss])
       })
+
+      c.subscribe("/message/system/info", (message) => {
+        setSysInfo(JSON.parse(message.body))
+      })
+
     })
 
   }
@@ -62,6 +79,7 @@ const Home: NextPage = () => {
           <h4>Hi there</h4>
           <button onClick={handleConnect}>Connect</button>
           <button onClick={handleDisconnect} >Disconnect</button>
+          <button onClick={getSystemInfo} >Get System info</button>
           <form onSubmit={handleSubmit}>
             <input value={value} onChange={handleOnChange} type="text" />
             <button type='submit' disabled={compatClient == undefined}>Send</button>
@@ -87,6 +105,17 @@ const Home: NextPage = () => {
 
             </tbody>
           </table>
+
+          <div style={{
+            marginTop: "1rem"
+          }}>
+            <p><strong>Available Processors</strong> : {sysInfo?.availableProcessor}</p>
+            <p><strong>Total Memory (GB)</strong> : {sysInfo?.totalMemory}</p>
+            <p><strong>Max memory (GB)</strong> : {sysInfo?.maxMemory}</p>
+            <p><strong>Free memory (GB)</strong> : {sysInfo?.freeMemory}</p>
+            <p><><strong>Time</strong> : {sysInfo?.time}</></p>
+
+          </div>
         </div>
       </div>
 
